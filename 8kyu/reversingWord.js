481062ce47f489e1377b217a535aa93c8a1e5330
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-09-06 19:51:09 +0200

    Create reversingWord.js

---
+++ 8kyu/reversingWord.js
@@ -0,0 +1,5 @@
+//You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+
+function reverse(string){
+  return string.split(' ').reverse().join(' ');
+}
