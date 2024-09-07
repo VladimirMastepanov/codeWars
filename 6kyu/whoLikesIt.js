510db05f62f96405c93e4b27b3f4aeb14d7a35e2
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-09-07 12:47:34 +0200

    Create whoLikesIt.js

---
+++ 6kyu/whoLikesIt.js
@@ -0,0 +1,34 @@
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. 
+// We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. 
+// It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+
+function likes(names) {
+  const length = names.length;
+  let res = '';
+  if (length === 0) {
+    res = 'no one likes this';
+  } 
+  if (length === 1) {
+    res = `${names[0]} likes this`;
+  }
+  if (length === 2) {
+    res =`${names[0]} and ${names[1]} like this`;
+  }
+  if (length === 3) {
+    res = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  }
+  if (length > 3) {
+    const friends = length - 2
+    res = `${names[0]}, ${names[1]} and ${friends} others like this`
+  }
+  return res;
+}
